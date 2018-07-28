ad97703b996cbcd50eae43ccbdcf9be466371d40
Mathieu <mathieu@woorank.com>
2018-07-28 15:54:22 +0200

    Add API data fetching for questions list feature

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,53 @@
+import axios from 'axios';
+
 import Layout from '../components/Layout';
 import QuestionCard from '../components/QuestionCard';
 
 const listContainerStyles = {
   display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'space-between',
   padding: '1%'
 };
 
-const Index = (props) => (
+const Index = ({questions, error}) => (
   <Layout>
     <div style={listContainerStyles}>
-      <QuestionCard />
+      {
+        error
+          ? <h2>There was an error while fetching the data. Please reload the page.</h2>
+          : null
+      }
+      {
+        questions
+        ? questions.map(({published_at, url, choices, question}) => (
+          <QuestionCard
+            publishedAt={published_at}
+            url={url}
+            choices={choices}
+            question={question}
+          />
+        ))
+        : null
+      }
     </div>
   </Layout>
 );
+
+Index.getInitialProps = async function () {
+  const res = await axios.get(`https://polls.apiblueprint.org/questions`);
+
+  if (res.status !== 200) {
+    return {
+      error: true,
+      questions: null
+    };
+  }
+
+  return {
+    error: false,
+    questions: res.data
+  };
+};
 
 export default Index;
